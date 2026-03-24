d2e5168c547fe76d257507aeb4e486d5ba991ae9
Gopika1705 <gopikajayaraman2005@gmail.com>
2026-03-24 13:51:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.java
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -1,113 +1,105 @@
-class MyLinkedList {
+class MyLinkedList{
     class Node{
         int val;
+        Node prev;
         Node next;
         Node(int val){
             this.val=val;
+            prev=null;
             next=null;
         }
     }
     Node head,tail;
     int cnt;
-    public MyLinkedList() {
+    public MyLinkedList(){
         head=tail=null;
         cnt=0;
     }
-    
-    public int get(int index) {
-        if(index<0 || index>=cnt)
-       {
-        return -1;
-       }
-       Node temp=head;
-       for(int i=0;i<index;i++)
-       {
-        temp=temp.next;
-       }
-       return temp.val; 
-    }
-    
-    public void addAtHead(int val) {
-        Node nn=new Node(val);
-        if(head==null)
-        {
-            head=nn;
-            tail=nn;
+    public int get(int index){
+        if(index<0 || index>=cnt){
+            return -1;
         }
-        else
-        {
-            nn.next=head;
-            head=nn;
+        Node temp=head;
+        for(int i=0;i<index;i++){
+            temp=temp.next;
         }
-        cnt+=1;
+        return temp.val;
     }
-    
-    public void addAtTail(int val) {
+    public void addAtHead(int val){
         Node nn=new Node(val);
-        if(head==null)
-        {
+        if(head==null){
             head=tail=nn;
         }
-        else
-        {
-            tail.next=nn;
-            tail=nn;
+        else{
+            nn.next=head;
+            head.prev=nn;
+            head=nn;
         }
         cnt+=1;
     }
-    public void addAtIndex(int index, int val) {
-        if(index<0 || index>cnt)
-        {
+    public void addAtTail(int val){
+        Node nn = new Node(val);
+        if(head==null){
+            head=tail=nn;
+        }
+        else{
+            tail.next=nn;
+            nn.prev=tail;
+            tail=nn;
+        }
+        cnt++;
+    }
+    public void addAtIndex(int index,int val){
+        if(index<0 || index>cnt){
             return;
         }
-        if(index==0)
-        {
+        if(index==0){
             addAtHead(val);
         }
-        else if(index==cnt)
-        {
+        else if(index==cnt){
             addAtTail(val);
         }
-        else
-        {
-             Node nn=new Node(val);
-             Node temp=head;
-             for(int i=0;i<index-1;i++)
-            {
-               temp=temp.next;
+        else{
+            Node temp=head;
+            for(int i=0;i<index-1;i++){
+                temp=temp.next;
             }
+            Node nn=new Node(val);
+            nn.prev=temp;
             nn.next=temp.next;
             temp.next=nn;
+            nn.next.prev=nn;
             cnt+=1;
         }
-
     }
-    
-    public void deleteAtIndex(int index) {
-        if(index<0 || index>=cnt)
-        {
+        public void deleteAtIndex(int index){
+             if(index<0 || index>=cnt){
             return;
         }
-        if(index==0)
-        {
+        if(index==0){
             head=head.next;
-            if(head==null)
-                tail=null;
+            if(head!=null) head.prev=null;
+            else tail=null;
+            cnt--;
         }
-        else
-        {
+        else if(index==cnt-1){
+            tail=tail.prev;
+            if(tail==null){
+                head=null;
+            }else{
+                tail.next=null;
+            }
+            cnt--;
+        }
+        else{
             Node temp=head;
-            for(int i=0;i<index-1;i++)
-            {
+            for(int i=0;i<index-1;i++){
                 temp=temp.next;
             }
             temp.next=temp.next.next;
-            if(temp.next==null)
-            {
-                tail=temp;
-            }
+            temp.next.prev=temp;
+            cnt--;
         }
-        cnt-=1;
     }
 }
 
