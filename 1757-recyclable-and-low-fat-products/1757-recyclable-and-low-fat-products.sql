74d4633f8b915facf99e376d133a75bfdc5f862f
Gopika1705 <gopikajayaraman2005@gmail.com>
2026-06-13 11:36:15 +0530

    Time: 587 ms (23.47%), Space: 0B (100%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select product_id 
 from Products
-where low_fats="y" 
-and recyclable="y"
+where low_fats='Y' 
+and recyclable='Y'
 order by product_id asc;
