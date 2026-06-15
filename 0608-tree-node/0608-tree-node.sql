05924177c7581f0a4dfeb6a11498deb7ba60a8ef
Gopika1705 <gopikajayaraman2005@gmail.com>
2026-06-15 17:39:36 +0530

    Time: 522 ms (28.57%), Space: 0B (100%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,10 @@
+SELECT t.id,
+       CASE
+           WHEN t.p_id IS NULL THEN 'Root'
+           WHEN c.p_id IS NULL THEN 'Leaf'
+           ELSE 'Inner'
+       END AS type
+FROM Tree t
+LEFT JOIN Tree c
+ON t.id = c.p_id
+GROUP BY t.id, t.p_id;
