715a5022520cc4d9502d89a00b724f34817aeb3f
Gopika1705 <gopikajayaraman2005@gmail.com>
2026-06-18 10:23:32 +0530

    Time: 4 ms (70.83%), Space: 55 MB (86.15%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,27 +1,29 @@
 class Solution {
     public boolean isChar(char c){
-        if((c>='A' && c<='Z') || (c>='a' && c<='z')){
+        if(c>='a' && c<='z'){
             return true;
         }
         return false;
     }
     public String processStr(String s) {
-        StringBuilder str=new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(isChar(s.charAt(i))){
-                str.append(s.charAt(i));
-            }
-            if(s.charAt(i)=='#'){
-                str.append(str);
-            }
-            if(s.charAt(i)=='*'){
-                if(str.length()>0)
+       StringBuilder str=new StringBuilder();
+       for(int i=0;i<s.length();i++){
+        char ch=s.charAt(i);
+        if(isChar(ch)){
+            str.append(ch);
+        }
+        else if(ch=='*'){
+            if(str.length()>0){
                 str.deleteCharAt(str.length()-1);
             }
-            if(s.charAt(i)=='%'){
-                str.reverse();
-            }
         }
-        return str.toString();
+        else if(ch=='#'){
+            str.append(str);
+        }
+        else if(ch=='%'){
+            str.reverse();
+        }
+       }
+       return str.toString();
     }
 }
