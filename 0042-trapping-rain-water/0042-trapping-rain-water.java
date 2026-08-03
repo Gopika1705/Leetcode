82d9d65cc8f0635d57dd63a115d0970abf0469ed
Gopika1705 <gopikajayaraman2005@gmail.com>
2026-08-03 16:31:05 +0530

    Time: 7 ms (4.35%), Space: 47.9 MB (45.46%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,18 +1,15 @@
 class Solution {
     public int trap(int[] height) {
-        if(height==null || height.length<3){
-            return 0;
-        }
-        int leftmax=height[0];
         Stack<Integer> s=new Stack<>();
+        int leftmax=height[0];
+        int water=0;
         s.push(height[height.length-1]);
         for(int i=height.length-2;i>=2;i--){
             s.push(Math.max(height[i],s.peek()));
         }
-        int water=0;
         for(int i=1;i<height.length-1;i++){
-            int minheight=Math.min(leftmax,s.peek());
-            water=water+Math.max(0,minheight-height[i]);
+            int min=Math.min(leftmax,s.peek());
+            water=water+Math.max(0,min-height[i]);
             s.pop();
             leftmax=Math.max(leftmax,height[i]);
         }
