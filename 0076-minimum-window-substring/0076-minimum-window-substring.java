e293f7576d56849323c737c1d644c942fe9bae75
Gopika1705 <gopikajayaraman2005@gmail.com>
2026-08-11 14:21:48 +0530

    Time: 2 ms (99.77%), Space: 45.5 MB (82.68%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,36 +1,37 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int freq[]=new int[128];
-        for(char ch:t.toCharArray()){
-            freq[ch]++;
-        }
-        int ws=0;
-        int minlen=Integer.MAX_VALUE;
-        int start=0;
-        int required=t.length();
-        for(int we=0;we<s.length();we++){
-           char ch=s.charAt(we);
-           if(freq[ch]>0){
+      int freq[]=new int[256];
+      for(int i=0;i<t.length();i++){
+        freq[t.charAt(i)]++;
+      }
+      int ws=0;
+      int minlen=Integer.MAX_VALUE;
+      int start=0;
+      int required=t.length();
+      for(int we=0;we<s.length();we++){
+        char c=s.charAt(we);
+        if(freq[c]>0){
             required--;
-           }
-           freq[ch]--;
-           while(required==0){
+        }
+        freq[c]--;
+        while(required==0){
             int wlen=we-ws+1;
             if(wlen<minlen){
                 minlen=wlen;
                 start=ws;
             }
-            char leftchar=s.charAt(ws);
-            freq[leftchar]++;
-            if(freq[leftchar]>0){
+            char l=s.charAt(ws);
+            freq[l]++;
+            if(freq[l]>0){
                 required++;
+
             }
             ws++;
-           }   
         }
-        if(minlen==Integer.MAX_VALUE){
+      }
+      if(minlen==Integer.MAX_VALUE){
             return "";
-        }
-        return s.substring(start,start+minlen);
+      }
+      return s.substring(start,start+minlen);
     }
 }
